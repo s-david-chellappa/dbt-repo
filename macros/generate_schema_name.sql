08d1c39f0fbffd6254b7ebae0b2d06481ba062d4
Sudhan Dhanaraj <sudhan.dhanaraj@tredence.com>
2026-03-19 10:44:21 +0000

    Commit 1

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,19 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- if custom_schema_name is none -%}
+
+    {%- set target_name = target.name | lower -%}
+
+    {%- if target_name == 'development' -%}
+        {# Personal schema — ignore all custom schemas #}
         {{ target.schema }}
+
     {%- else -%}
-        {{ custom_schema_name | trim }}
+        {# Dev / Prod — respect dbt_project.yml schema definitions #}
+        {%- if custom_schema_name is not none -%}
+            {{ custom_schema_name | trim }}
+        {%- else -%}
+            {{ target.schema }}
+        {%- endif -%}
+
     {%- endif -%}
+
 {%- endmacro %}
